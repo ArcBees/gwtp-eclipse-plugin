6bf47aa01e5c4730ef6c9e60753d7082736203b9
michaelrenaud.qc@gmail.com <michaelrenaud.qc@gmail.com>
2011-02-22 16:08:14 -0500

    Copyright

--- com.imagem.gwtpplugin/src/com/imagem/gwtpplugin/projectfile/src/server/guice/GuiceServletContextListener.java
+++ com.imagem.gwtpplugin/src/com/imagem/gwtpplugin/projectfile/src/server/guice/GuiceServletContextListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 Les Systèmes Médicaux Imagem Inc.
+ * Copyright 2011 IMAGEM Solutions TI santé
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
