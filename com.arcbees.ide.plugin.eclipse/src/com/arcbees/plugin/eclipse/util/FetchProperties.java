8490e9aa80a20ca4e28a8f342e6ce86c3a15f8d3
Brandon Donnelson <branflake2267@gmail.com>
2013-05-17 22:44:22 -0700

    Added copyrights.

--- com.arcbees.ide.plugin.eclipse/src/com/arcbees/plugin/eclipse/util/FetchProperties.java
+++ com.arcbees.ide.plugin.eclipse/src/com/arcbees/plugin/eclipse/util/FetchProperties.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
 package com.arcbees.plugin.eclipse.util;
 
 import java.net.MalformedURLException;
@@ -14,24 +31,25 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 public class FetchProperties {
     public enum Config {
         FILE, REQUIRED, TASKS, OPTIONAL;
-        
+
         public String toString() {
             return name().toLowerCase();
         }
     }
-    
+
     private String path;
     private PropertiesConfiguration configuration;
 
     public FetchProperties(String path) {
         this.path = path;
     }
-    
+
     /**
-     * TODO retry on failures
+     * TODO retry on failures 
      * TODO report error
-     * @throws MalformedURLException 
-     * @throws ConfigurationException 
+     * 
+     * @throws MalformedURLException
+     * @throws ConfigurationException
      */
     public PropertiesConfiguration fetch() throws MalformedURLException, ConfigurationException {
         URL url;
@@ -40,34 +58,34 @@ public class FetchProperties {
         } catch (MalformedURLException e) {
             throw e;
         }
-        
+
         try {
             configuration = new PropertiesConfiguration(url);
         } catch (ConfigurationException e) {
             throw e;
         }
-        
+
         System.out.println(configuration.toString());
-        
+
         return configuration;
     }
-    
+
     public PropertiesConfiguration getConfiguration() {
         return configuration;
     }
-    
+
     public List<String> getFiles() {
         return getList(Config.FILE);
     }
-    
+
     public List<String> getRequired() {
         return getList(Config.FILE);
     }
-    
+
     public List<String> getTasks() {
         return getList(Config.TASKS);
     }
-    
+
     public List<String> getOptional() {
         return getList(Config.OPTIONAL);
     }
@@ -75,12 +93,12 @@ public class FetchProperties {
     private List<String> getList(Config config) {
         if (configuration == null) {
             System.out.println("Did you fecth... exiting...");
-            throw(new NullPointerException());
+            throw (new NullPointerException());
         }
-        
+
         String key = config.toString();
         List<Object> objects = configuration.getList(key);
-        
+
         List<String> strings = new ArrayList<String>();
         for (Object object : objects) {
             if (object != null) {
--- com.arcbees.ide.plugin.eclipse/src/com/arcbees/plugin/eclipse/wizard/createpresenter/CreatePresenterPage.java
+++ com.arcbees.ide.plugin.eclipse/src/com/arcbees/plugin/eclipse/wizard/createpresenter/CreatePresenterPage.java
@@ -1,18 +1,32 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.arcbees.plugin.eclipse.wizard.createpresenter;
 
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import com.arcbees.plugin.eclipse.domain.PresenterConfigModel;
-
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Button;
-import swing2swt.layout.FlowLayout;
 
 public class CreatePresenterPage extends WizardPage {
     private PresenterConfigModel presenterConfigModel;
