0b1e7a73a5f2fdec1567076e568afe9a7e661fa9
Richard Wallis <rdwallis@gmail.com>
2014-09-16 12:25:34 +0200

    Revert "Complete Rewrite"

---
+++ com.arcbees.plugin.eclipse/src/com/arcbees/plugin/eclipse/domain/ProjectConfigModel.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.arcbees.plugin.eclipse.domain;
+
+import org.eclipse.swt.widgets.Shell;
+
+public class ProjectConfigModel extends ModelObject {
+    private Shell shell;
+    private String projectName;
+    private String packageName;
+    private String moduleName;
+    private String groupId;
+    private String artifactId;
+    private String projectPath;
+    private Archetype archetypeSelected;
+    private String workspacePath;
+    
+    public ProjectConfigModel(Shell shell) {
+        this.shell = shell;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        firePropertyChange("packageName", this.packageName, this.packageName = packageName);
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        firePropertyChange("moduleName", this.moduleName, this.moduleName = moduleName);
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        firePropertyChange("groupId", this.groupId, this.groupId = groupId);
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        firePropertyChange("artifactId", this.artifactId, this.artifactId = artifactId);
+    }
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        firePropertyChange("projectPath", this.projectPath, this.projectPath = projectPath);
+    }
+    
+    public Archetype getArchetypeSelected() {
+        return archetypeSelected;
+    }
+
+    public void seArchetypeSelected(Archetype archetypeSelected) {
+        firePropertyChange("archetypeSelected", this.archetypeSelected, this.archetypeSelected = archetypeSelected);
+    }
+    
+    @Override
+    public String toString() {
+        String s = "{ ProjectConfigModel: ";
+        s += "projectName=" + projectName + " ";
+        s += "packageName=" + packageName + " ";
+        s += "moduleName=" + moduleName + " ";
+        s += "groupId=" + groupId + " ";
+        s += "artifactId=" + artifactId + " ";
+        s += "projectPath=" + projectPath + " ";
+        s += "archetypeSelected=" + archetypeSelected + " ";
+        s += " }"; 
+        return s;
+    }
+
+    // TODO future
+    public String getVersion() {
+        return "1.0-SNAPSHOT";
+    }
+
+    // TODO future
+    public String getDescription() {
+        return "This project was genereted by ArcBees Eclipse plugin.";
+    }
+
+    public boolean canBeFinished() {
+        return archetypeSelected != null;
+    }
+
+    public void setWorkspacePath(String workspacePath) {
+        this.workspacePath = workspacePath;
+    }
+    
+    public String getWorkspacePath() {
+        return workspacePath;
+    }
+    
+    public Shell getShell() {
+        return shell;
+    }
+}
---
+++ com.arcbees.plugin.eclipse/src/com/arcbees/plugin/eclipse/util/FetchProperties.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2013 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.arcbees.plugin.eclipse.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+/**
+ * When tycho is used, use maven to get this class from IDE-Templates
+ */
+public class FetchProperties {
+    public enum Config {
+        FILE, REQUIRED, TASKS, OPTIONAL;
+
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
+    private String path;
+    private PropertiesConfiguration configuration;
+
+    public FetchProperties(String path) {
+        this.path = path;
+    }
+
+    /**
+     * TODO retry on failures 
+     * TODO report error
+     * 
+     * @throws MalformedURLException
+     * @throws ConfigurationException
+     */
+    public PropertiesConfiguration fetch() throws MalformedURLException, ConfigurationException {
+        URL url;
+        try {
+            url = new URL(path);
+        } catch (MalformedURLException e) {
+            throw e;
+        }
+
+        try {
+            configuration = new PropertiesConfiguration(url);
+        } catch (ConfigurationException e) {
+            throw e;
+        }
+
+        System.out.println(configuration.toString());
+
+        return configuration;
+    }
+
+    public PropertiesConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public List<String> getFiles() {
+        return getList(Config.FILE);
+    }
+
+    public List<String> getRequired() {
+        return getList(Config.FILE);
+    }
+
+    public List<String> getTasks() {
+        return getList(Config.TASKS);
+    }
+
+    public List<String> getOptional() {
+        return getList(Config.OPTIONAL);
+    }
+
+    private List<String> getList(Config config) {
+        if (configuration == null) {
+            System.out.println("Did you fecth... exiting...");
+            throw (new NullPointerException());
+        }
+
+        String key = config.toString();
+        List<Object> objects = configuration.getList(key);
+
+        List<String> strings = new ArrayList<String>();
+        for (Object object : objects) {
+            if (object != null) {
+                strings.add(object.toString());
+            }
+        }
+        return strings;
+    }
+}
